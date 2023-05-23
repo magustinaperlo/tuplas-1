219edda4f8ebad32a2c5877d8bd136f57b5e3b17
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2023-05-23 19:23:36 -0300

    Update_tuplas_1.py

--- tuplas_1.py
+++ tuplas_1.py
@@ -16,3 +16,18 @@ else:
         print(meses[num-1])  #le restamos uno porque los meses empiezan desde la posicion 0
     else:
         print("El numero se encuentra fuera del rango de posiciones")
+
+#SUGERENCIA CON CORRECCION: Pequeña mejora que pODES hacer en la línea 
+#donde verificas si el usuario ingresó un cero para salir del programa.En lugar de usar exit como una función,
+·#pones break para salir del bucle y finalizar el programa.
+
+meses = ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+
+while True:
+    num = int(input("Ingrese un número: "))
+    if num == 0:
+        break
+    elif 1 <= num <= len(meses):
+        print(meses[num-1])
+    else:
+        print("El número se encuentra fuera del rango de posiciones")
